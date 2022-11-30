d9ed6f72b8f993bde01c161369a1d8a3afdfade7
edureka <support@edureka.co>
2022-11-30 01:43:51 +0000

    Corrected Syntax in Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple{
     public static void main(String args[]){
-    System.out.println("Welcome to Git's Online Training programme")
+    System.out.println("Welcome to Git's Online Training programme");
     }
 }
